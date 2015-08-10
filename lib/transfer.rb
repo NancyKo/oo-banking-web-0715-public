db5c7cc872eabfa2f6b98c96b7980f923b9e86d7
N K <konancys@gmail.com>
2015-08-09 20:53:13 -0400

    Adding Transfer class, what is complete so far

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Transfer
   # code here
+attr_accessor :name, :from, :amount, :status
+
+	def self.scan
+		
+	end
+
+  def initialize(name, from, amount)
+  	@name = name
+  	@from = from
+  	@amount = amount
+  	@status = 'pending'
+  end
+
+  def sender
+  	@name
+  end
+
+  def receiver
+  	@from
+  end 
+
+  def both_valid?
+  	@name.valid? && @from.valid?
+  end
 end
